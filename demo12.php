42d15288c644be16d98d7b3269e2c1c63b662f01
v3vinay <vinay@techfino.in>
2021-12-24 15:29:20 +0530

    first commit

---
+++ demo12.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<?php
+$age= ("22");
+if ($age < "18") {
+  echo "Vaccine is yet to develop!";
+} 
+ elseif($age >= "18" && $age < "45"){
+ echo "Vaccine starts in 15 days";
+ }
+else {
+  echo "Vaccine started , Get vaccinated soon";
+}
+?>
+</body>
+</html>
